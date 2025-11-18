1eeb178b53a805c98a1a4bb0c08afe6ced6d5847
cristog <cristo.g@outlook.es>
2025-11-18 20:19:53 +0100

    parte 1

--- neural_network/src/main.py
+++ neural_network/src/main.py
@@ -10,15 +10,21 @@ Y_train = idx2numpy.convert_from_file("../train-labels-idx1-ubyte")
 X_test = X_test / 255
 X_train = X_train / 255
 
+X_test_flat = X_test.reshape(X_test.shape[0], -1)
 
+
+# hace falta aplanar la x para poder hacer la multiplicacion en el forward
 def relu(x):
+    """
+    La funcion de Activacion
+    """
     if x > 0:
-        return input
+        return x
     else:
         return 0
 
 
-# filas 784 y el batch size
+# filas 784 y el numero de neuronas que es la salida
 def inicializar(entrada, salida):
     w = np.random.randn(salida, entrada)
     b = np.random.randn(1, salida)
@@ -26,4 +32,9 @@ def inicializar(entrada, salida):
     return w, b
 
 
-w, b = inicializar(784, 10)  # el 100 es el batch size
+def forward(x, w, b):
+    return x @ w + b  #
+
+
+w, b = inicializar(784, 100)  # el 100 es la capa oculta hidden layer
+print(forward(X_test, w, b))

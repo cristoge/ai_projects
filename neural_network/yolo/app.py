de5ca403acbc756db0a8a1e633c255552677a990
cristoge <cristo.g@outlook.es>
2026-03-19 13:04:59 +0100

    filtros mejor ordenados

--- neural_network/yolo/app.py
+++ neural_network/yolo/app.py
@@ -1,10 +1,13 @@
 import supervision as sv
 import cv2
+from supervision.draw.color import Color
 from ultralytics import YOLO
 import numpy as np
 
 # Carga el modelo
 model = YOLO("yolo11n.pt")
+tracker = sv.ByteTrack(minimum_consecutive_frames=3)
+tracker.reset()
 # Inicializa el anotador de cajas
 bounding_box_annotator = sv.BoxAnnotator()  # ya no necesita argumentos
 POLYGON = np.array(
@@ -27,23 +30,48 @@ POLYGON = np.array(
 )
 polygon_zone = sv.PolygonZone(polygon=POLYGON, triggering_anchors=(sv.Position.CENTER,))
 
+CLASSES = [2, 3]
+label_annotator = sv.LabelAnnotator(text_color=Color.BLUE)
+trace_annotator = sv.TraceAnnotator(trace_length=10)
+
 
 def main():
     video_file_path = "./video.mp4"
 
-    frame_generator = sv.get_video_frames_generator(source_path=video_file_path)
+    frame_generator = sv.get_video_frames_generator(
+        source_path=video_file_path, stride=2
+    )
 
     for i, frame in enumerate(frame_generator):
         # Procesa el frame con YOLO
+        # YOLO
         result = model(frame, device="cuda", verbose=False, imgsz=1280)[0]
         detections = sv.Detections.from_ultralytics(result)
+
+        # filtro zona
         detections = detections[polygon_zone.trigger(detections)]
+
+        # filtro clases
+        detections = detections[(detections.class_id == 2) | (detections.class_id == 3)]
+
+        # tracking
+        detections = tracker.update_with_detections(detections)
+
+        # labels (AHORA sí correcto)
+        labels = [f"#{tid}" for tid in detections.tracker_id]
         # Anota las detecciones en una copia del frame
+        detections = detections[(detections.class_id == 2) | (detections.class_id == 3)]
         annotated_frame = bounding_box_annotator.annotate(
             scene=frame.copy(), detections=detections
         )
         annotated_frame = sv.draw_polygon(
             scene=annotated_frame, polygon=POLYGON, color=sv.Color.RED, thickness=2
+        )
+        annotated_frame = label_annotator.annotate(
+            scene=annotated_frame, detections=detections, labels=labels
+        )
+        annotated_frame = trace_annotator.annotate(
+            scene=annotated_frame, detections=detections
         )
         cv2.imshow("Processed Video", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -52,5 +80,4 @@ def main():
     cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
-    main()
+main()
